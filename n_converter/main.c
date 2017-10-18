753649abeb71a4d9b430e62efea981f603bb87ed
Vijay <alterechoxii@gmail.com>
2017-10-18 23:07:28 +0530

    made conversion prog accept args

--- n_converter/main.c
+++ n_converter/main.c
@@ -1,9 +1,23 @@
 #include <stdio.h>
-
+#include <string.h>
 #include "bin_dec.h"
 
 int main(int argc, char **argv) {
-	long n = decFromBinStr("1111");
-	printf("\ndec=%ld\n", n);	
+
+	if (argc == 1) {
+		printf("no arguments given\n");
+		return 1;
+	} else if (argc == 2) {
+		printf("not enough conversion details\n");
+		return 1;
+	}
+
+	if (strcmp("-bd", argv[1]) == 0) {
+		printf("\ndecimal value = %ld\n", decFromBinStr(argv[2]));
+	} else {
+		printf("unrecognised conversion specifier '%s'", argv[1]);
+	}
+
+
 	return 0;
 }
