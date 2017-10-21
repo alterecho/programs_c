d31012bfc9499429f3d57056be1414c1af06b296
vijaychandran <vjc@ventunotech.com>
2017-10-21 18:56:41 +0530

    added -dh -dx params

--- n_converter/main.c
+++ n_converter/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include "bin_dec.h"
 #include "hex_dec.h"
@@ -20,6 +21,9 @@ int main(int argc, char **argv) {
 
 	if (strcmp("-bd", argv[1]) == 0) {
 		printf("\ndecimal value = %ld\n", decFromBinStr(argv[2]));
+	} else if (strcmp("-dh", argv[1]) == 0 || strcmp("-dx", argv[1]) == 0) {
+		int n = strtol(argv[2], NULL, 10);
+		printf("\ndecimal value = %s\n", hexStrFromDec(n));
 	} else {
 		printf("unrecognised conversion specifier '%s'", argv[1]);
 	}
