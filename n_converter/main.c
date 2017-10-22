0897853893a96998b38189be6332e7919dc0e6b6
Vijay <alterechoxii@gmail.com>
2017-10-22 10:19:17 +0530

    added comments in main.c + cleanup

--- n_converter/main.c
+++ n_converter/main.c
@@ -6,28 +6,32 @@
 
 int main(int argc, char **argv) {
 
-	unsigned int n = 100;
-	printf("\nhex:%x, dec:%d\n", n, n);
-	
-	char *str = hexStrFromDec(1200);
-
+	/* return if there are no arguments */
 	if (argc == 1) {
 		printf("no arguments given\n");
 		return 1;
-	} else if (argc == 2) {
+	}
+	/* return if there are not enough arguments (conversion type and value) */
+	else if (argc == 2) {
 		printf("not enough conversion details\n");
 		return 1;
 	}
 
+	
+	/* binary -> decimal */
 	if (strcmp("-bd", argv[1]) == 0) {
 		printf("\ndecimal value = %ld\n", decFromBinStr(argv[2]));
-	} else if (strcmp("-dh", argv[1]) == 0 || strcmp("-dx", argv[1]) == 0) {
+	}
+	/* decimal -> hex */
+	else if (strcmp("-dh", argv[1]) == 0 || strcmp("-dx", argv[1]) == 0) {
 		int n = strtol(argv[2], NULL, 10);
 		printf("\ndecimal value = %s\n", hexStrFromDec(n));
-	} else {
+	}
+       /* unrecognized */
+	else {
 		printf("unrecognised conversion specifier '%s'", argv[1]);
 	}
 
-
+	printf("\n");
 	return 0;
 }
