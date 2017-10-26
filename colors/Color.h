7e89857d55212ef3bfbbfb8a24dc32df1a6f2270
vijaychandran <vjc@ventunotech.com>
2017-10-26 17:07:27 +0530

    added ColorZero method

--- colors/Color.h
+++ colors/Color.h
@@ -15,6 +15,8 @@ static inline Color ColorZero() {
 	return c;
 }
 
+typedef ColorZeror() ColorZero;
+
 Color colorWith(unsigned char red, unsigned char green, unsigned char blue, unsigned char alpha);
 Color colorForHex(const char *str);
 void printColor(Color color, const char *tag);
